2f2e55391dce228d30026f30bbff700b9a87c037
zhaoweibo@longruan.com <609697140@qq.com>
2018-09-25 10:43:01 +0800

    update 20180925

--- XJBTAJPT/src/XJBT/all_test.py
+++ XJBTAJPT/src/XJBT/all_test.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             print_exc()
     print ('Running the tests...')
     
-filename = 'D:\\XJBT_Result_20180919.html'
+filename = 'D:\\XJBT_Result_20180925.html'
 fp = open(filename, 'wb')
 
 runner =HTMLTestRunner.HTMLTestRunner(
